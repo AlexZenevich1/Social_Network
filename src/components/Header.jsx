fc142aef08ddbf916819935f7492a6c5f8a3cc45
AlexZenevich <58400324+AlexZenevich@users.noreply.github.com>
2021-01-30 13:25:58 +0300

    'first'

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className='header'><header className='header'><p className='logo'><span className='logoname'>𝕬𝖑𝖊𝖝</span>𝖅𝖊𝖓𝖊𝖛𝖎𝖈𝖍</p></header></div>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+
+function Profile(){
+    return(
+        <div className='content'>
+        <div>  <img src='https://static5.depositphotos.com/1007593/453/i/600/depositphotos_4530089-stock-photo-african-landscape.jpg'></img></div>
+        <div>
+        ava + description
+      </div>
+      <div>
+        My post
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+      </div>
+      
+    )
+  
+}
+
+export default Profile;
